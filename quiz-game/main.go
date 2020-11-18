32d73bfe0596f7631657d733fed8bcf372ad9e9c
Patrick Furtak <patrick@patrickfurtak.com>
2020-11-18 15:48:42 -0500

    Brought in readme and problem set, Opened and parsed csv file while also handling csv read errors with an exit status of 1

--- quiz-game/main.go
+++ quiz-game/main.go
@@ -1,5 +1,31 @@
 package main
 
-func main() {
+import (
+	"encoding/csv"
+	"flag"
+	"fmt"
+	"os"
+)
 
+func main() {
+	csvFilename := flag.String("csv", "problems.csv", "a csv file in the format of 'question,answer'")
+	flag.Parse()
+
+	file, err := os.Open(*csvFilename)
+	if err != nil {
+		exit(fmt.Sprintf("Failed to open csv file problem set: '%s'\n", *csvFilename))
+	}
+
+	r := csv.NewReader(file)
+	lines, err := r.ReadAll()
+	if err != nil {
+		exit("Failed to parse provided csv file.")
+	}
+
+	fmt.Println(lines)
+}
+
+func exit(msg string) {
+	fmt.Println(msg)
+	os.Exit(1)
 }
